042ecac720971b1e0a65ee1390155e0f4c2392ad
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-21 09:26:19 +0200

    Ajout première fonction de tri.

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ const inventeurs = [
     { first: "Isaac", last: "Newton", year: 1643, mort: 1727 },
     { first: "Galileo", last: "Galilei", year: 1564, mort: 1642 },
     { first: "Marie", last: "Curie", year: 1867, mort: 1934 },
-    { first: "Johannes", last: "Kepler", year: 1571, mort: 11630 },
+    { first: "Johannes", last: "Kepler", year: 1571, mort: 1630 },
 ]
 
 
 const personne = ["Beck, Glenn", "Becker, Carl", "Beckett, Samuel", "Beddoes, Mick", "Beecher, Henry", "Beethoven, Ludwig", "Begin, Menachem", "Belloc, Hilaire", "Bellow, Saul", "Benchley,Robert", "Benenson, Peter", "Ben-Gurion, David", "Benjamin, Walter", "Benn, Tony", "Bennington, Chester", "Benson, Leana", "Bent, Silas", "Bentsen, Lloyd", "Berger, Ric", "Bergman, Ingmar", "Berio, Luciano", "Berle, Milton", "Berling, Irving", "Berne, Eric", "Bernhard, Sandra", "Berra, Yogi", "Berry, Halle", "Berry, Wendell", "Betea, Erin", "Bevan, Aneurin", "Bevel, Ken", "Biden, Joseph", "Bierce, Ambrose", "Biko, Steve", "Billings, Josh", "Biondo, Frank", "Birrell, Augustine", "Black, Elk", "Blair, Robert", "Blair, Tony", "Blake, William"];
+
+
+
+const fifteen = inventeurs.filter(function(inventeur) {
+    if(inventeur.year >= 1500 && inventeur.year <= 1600) {
+        return true; // tu le gardes !
+    }
+})
+console.log (fifteen);
